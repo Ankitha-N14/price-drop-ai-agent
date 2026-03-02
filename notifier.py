d9508ad79a03de24da5783390fca481b554c6e70
Ankitha Nambiar <nankitha14@gmail.com>
2026-03-02 19:53:33 +0530

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,1 +1,17 @@
+import smtplib
+import os
+from email.message import EmailMessage
 
+EMAIL_USER = os.environ.get("EMAIL_USER")
+EMAIL_PASS = os.environ.get("EMAIL_PASS")
+
+def send_email(subject, content):
+    msg = EmailMessage()
+    msg["From"] = EMAIL_USER
+    msg["To"] = EMAIL_USER
+    msg["Subject"] = subject
+    msg.set_content(content)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL_USER, EMAIL_PASS)
+        server.send_message(msg)
