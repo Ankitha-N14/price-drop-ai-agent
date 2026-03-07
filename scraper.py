a54ed92e9b7cbc81e28dc519e7743987fb759e67
Ankitha Nambiar <nankitha14@gmail.com>
2026-03-07 08:29:44 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,12 +7,13 @@ import random
 def get_price(url):
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
+        "User-Agent": "Mozilla/5.0",
         "Accept-Language": "en-US,en;q=0.9"
     }
 
     try:
         time.sleep(random.randint(3,8))
+
         response = requests.get(url, headers=headers, timeout=10)
 
         soup = BeautifulSoup(response.text, "html.parser")
@@ -20,7 +21,7 @@ def get_price(url):
         price_tag = soup.select_one("span.a-price span.a-offscreen")
 
         if price_tag:
-            price = price_tag.text.replace("₹", "").replace(",", "").strip()
+            price = price_tag.text.replace("₹","").replace(",","").strip()
             return float(price)
 
         if "Currently unavailable" in response.text:
